a75eae483c6f80281b07944cdb351e556d6e6fcf
Artur <sky@crucially.net>
2010-05-13 13:48:55 -0700

    command line options

--- riak-new.pl
+++ riak-new.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl;
+
 
 use strict;
 use warnings;
@@ -5,15 +7,36 @@ use warnings;
 use lib '.';
 use RiakFuse;
 
+
+use Getopt::Long;
+
+my $fsname;
+my $server;
+my $clean = 0;
+my $mountpoint;
+my $trace = 0;
+my $debug = 0;
+GetOptions("clean"     => \$clean,
+	   "server=s"  => \$server,
+	   "fsname=s"  => \$fsname,
+	   "mount=s"   => \$mountpoint,
+	   "trace=i"   => \$trace,  
+	   "debug=i"   => \$debug,  
+    );
+
+die "Please tell me what server I should be using (--server=127.0.0.1:8091)\n" unless $server;
+die "Please tell me the name of the filesystem (--fsname=mytestfs) (maps to a bucket)\n" unless $fsname;
+die "Please tell me where to mount it (--mount=/tmp/myfs) (where to mount)\n" unless $mountpoint;
+
 RiakFuse::conf(
     mountpoint => "/tmp/foo",
-    trace => 2,
-    trace => 2,
-    threaded => 0,
+    trace => $trace,
+    debug => $debug,
+    threaded => 1,
     bufferdir => "/tmp/buffer",
-    filebucket => "filesystem1",
+    filebucket => "$fsname",
     logbucket => "filesystem1_log",
-    server => "http://127.0.0.1:8091/",
+    server => "http://$server/",
     );
 
 
