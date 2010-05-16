f06d9a0bb5133ffd80db227b4a2749c8b729853f
Artur <sky@crucially.net>
2010-05-16 16:55:49 +0100

    implement a cache in the data get layer

--- RiakFuse/Data.pm
+++ RiakFuse/Data.pm
@@ -3,33 +3,60 @@ use strict;
 use warnings;
 package RiakFuse::Data;
 use RiakFuse::HTTP;
-
+use threads::shared;
+use Data::Dumper;
 my %on_disk;
 
+my %cache;
+my %inverse_cache;
 
 sub fetch {
     my $class = shift;
     my $file = shift;
     my $method = shift;
-
+    my $cache_ok = shift || 0;
     my $obj;
-    if(my $cached = $on_disk{$file->key}) {
-	$obj = RiakFuse::HTTP->$method($file->key, $on_disk{$file->key}->{'etag'});
-#	print "Conditional get worked\n" if($obj == 0);
-	return $cached if($obj == 0);
-	return $obj unless ref $obj;
+
+    if(my $neg = $inverse_cache{$file->key}) {
+	if($neg->{stored} >= time) {
+	    RiakFuse::Stats->increment("data_fetch_error_cached");
+	    return $neg->{error};
+	}
+	delete $inverse_cache{$file->key};
+    }
+
+    my $cached = $cache{$file->key};
+    if($cached &&
+	(($method eq 'get' && exists $cached->{content}) || $method eq 'head')) {
+	if($cache_ok && $cached->{stored} >= time) {
+	    RiakFuse::Stats->increment("data_fetch_cached");
+	    return $cached;
+	}
+	RiakFuse::Stats->increment("data_fetch_cond_attempt");
+	$obj = RiakFuse::HTTP->$method($file->key, $cached->{'etag'});
+	if($obj == 0) {
+	    $cached->{stored} = time;
+	    RiakFuse::Stats->increment("data_fetch_cond_success");
+	    return $cached;
+	}
     } else {
 	$obj = RiakFuse::HTTP->$method($file->key); 
     }
-    return $obj unless (ref($obj));
-    return $obj if ($method eq 'head');
 
-
+    unless (ref($obj)) {
+	$inverse_cache{$file->key} = {
+	    error => $obj,
+	    stored => time};
+	RiakFuse::Stats->increment("data_fetch_error");
+	return $obj;
+    }
     
+    RiakFuse::Stats->increment("data_fetch_success");
 
-    $on_disk{$file->key} = $obj;
+    $cache{$file->key} = $obj;
+    $cache{$file->key}->{stored} = time;
     
-    return $obj;
+    return $cache{$file->key};
 
 }
 
@@ -37,13 +64,15 @@ sub fetch {
 sub head {
     my $class = shift;
     my $file = shift;
-    $class->fetch($file, 'head');
+    my $cached_ok = shift;
+    $class->fetch($file, 'head', $cached_ok);
 }
 
 sub get {
     my $class = shift;
     my $file = shift;
-    $class->fetch($file, 'get');
+    my $cached_ok = shift;
+    $class->fetch($file, 'get', $cached_ok);
 }
 
 sub put {
@@ -51,14 +80,16 @@ sub put {
     my $file  = shift;
     my $mime  = shift;
     my $obj   = shift;
-    delete ($on_disk{$file->key});
+    delete ($cache{$file->key});
+    delete($inverse_cache{$file->key});
     return RiakFuse::HTTP->put($file->key, $mime,  $obj);
 }
 
 sub delete {
     my $class = shift;
     my $file  = shift;
-    delete($on_disk{$file->key});
+    delete($cache{$file->key});
+    delete($inverse_cache{$file->key});
     return RiakFuse::HTTP->delete($file->key);
 }
 1;
