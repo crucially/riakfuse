60321dea961d52948ba8248655169988c4d034ed
Artur <sky@crucially.net>
2010-06-11 13:37:27 -0700

    merge the actions on read

--- RiakFuse/MetaData/Directory.pm
+++ RiakFuse/MetaData/Directory.pm
@@ -34,20 +34,34 @@ sub get {
     my $path = shift;
     $path = $path->key if (ref($path));
     print "Fetching $path\n";
-    my $request = HTTP::Request->new("HEAD", $conf->mdurl . $path);
-    
-    my $response = LWP::UserAgent->new->request($request);
-    my %directory : shared;
-    my $directory = \%directory;
-    foreach my $header (keys %RiakFuse::MetaData::headers) {
-	$directory->{$RiakFuse::MetaData::headers{$header}} = $response->header($header);
+    my $request = HTTP::Request->new("GET", $conf->mdurl . $path);
 
+    for (1..100) {
+	# make sure we can get all copies in here
+	$request->header("Accept", "multipart/mixed, */*;q=0.5");
+
+	my $response = LWP::UserAgent->new->request($request);
+
+	if($response->code == 300) {
+	    # need to merge the different changes and then restart
+	    $self->merge($conf, $response, $path);
+	    next;
+	}
+
+
+	my %directory : shared;
+	my $directory = \%directory;
+	foreach my $header (keys %RiakFuse::MetaData::headers) {
+	    $directory->{$RiakFuse::MetaData::headers{$header}} = $response->header($header);
+	    
+	}
+	$directory->{link} = $response->header("Link");
+	$directory->{mtime} = str2time($response->header("Last-Modified"));
+
+	my $class = ref($self) || $self;
+	bless $directory, $class;
+	return $directory;
     }
-    $directory->{link} = $response->header("Link");
-    $directory->{mtime} = str2time($response->header("Last-Modified"));
-    my $class = ref($self) || $self;
-    bless $directory, $class;
-    return $directory;
 }
 
 sub save {
@@ -68,14 +82,79 @@ sub add {
 
     my $request = HTTP::Request->new("POST", $conf->mdurl . $parent->{key});
     
-    foreach my $header (keys %RiakFuse::MetaData::headers_r) {
-	$request->header($RiakFuse::MetaData::headers_r{$header}, $parent->{$header});
-	
-    }
+#    foreach my $header (keys %RiakFuse::MetaData::headers_r) {
+#	$request->header($RiakFuse::MetaData::headers_r{$header}, $parent->{$header});
+#	
+#    }
     $request->header("Link", "<" . "/riak/" . $conf->mdbucket . "/" .$child->{key}  . '>; riaktag="child"');
+    $request->header("X-Riak-Meta-Rfs-Action" , "add");
     $request->header("Content-Type", "text/plain");
     print $request->as_string;
 
     print LWP::UserAgent->new()->request($request)->as_string
 }
+
+sub merge {
+    my $class = shift;
+    my $conf = shift;
+    my $response = shift;
+    my $key = shift;
+
+    my $vclock = $response->header("X-Riak-Vclock");
+    my @parts = $response->parts;
+
+    my %links;
+    my %links_new;
+
+
+
+    my $request = HTTP::Request->new("POST", $conf->mdurl. $key);
+    $request->header("X-Riak-Vclock", $vclock);
+    
+    foreach my $part (@parts) {
+	no warnings;
+	foreach my $link (split "," , $part->header("Link")) {
+	    my ($path) = $link =~/\<(.+)\>; riaktag=\"child\"/;
+	    next unless $path;
+	    if ($part->header("X-Riak-Meta-Rfs-Action") eq 'create') {
+		$links_new{$path}++;
+	    } else {
+		$links{$path}++;
+	    }
+	}
+
+	
+	if ($part->header("X-Riak-Meta-Rfs-Action") eq '') {
+	    # we are merging against this
+	    foreach my $header (keys %RiakFuse::MetaData::headers) {
+		$request->header($header, $part->header($header));
+	    }	    
+	}
+
+    }
+
+    foreach my $new (keys %links_new) {
+	$links{$new}++;
+
+    }
+
+
+
+    my $buffer = "";
+
+    foreach my $link (keys %links) {
+	$buffer .= "<$link>; riaktag=\"child\" ,";
+	if( length($buffer) > 7000) {
+	    $request->push_header("Link", $buffer);
+	    $buffer .= "";
+	}
+    }
+    $request->push_header("Link", $buffer) if($buffer);
+    $request->header("Content-Type", "text/plain");
+
+
+    print $request->as_string;
+    print LWP::UserAgent->new()->request($request)->as_string;
+}
+
 1;
