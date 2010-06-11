e2f7123f1d9b4e84f29a884901a800219335ee56
Artur <sky@crucially.net>
2010-06-11 12:49:56 -0700

    conversion between HTTP headers and internal fields, not sure why they aren't just the same

--- RiakFuse/MetaData.pm
+++ RiakFuse/MetaData.pm
@@ -6,5 +6,23 @@ use warnings;
 
 
 
+our %headers = (
+    'X-Riak-Meta-RFS-ctime'  => 'ctime',
+    'X-Riak-Meta-RFS-uid'    => 'uid',
+    'X-Riak-Meta-RFS-gid'    => 'gid',
+    'X-Riak-Meta-RFS-mode'   => 'mode',
+    'X-Riak-Meta-RFS-type'   => 'type',
+    'X-Riak-Meta-RFS-key'    => 'key',
+    'X-Riak-Meta-RFS-name'   => 'name',
+    'X-Riak-Meta-RFS-parent'   => 'parent',
+    );
+
+our %headers_r;
+
+foreach my $header (keys %headers) {
+    $headers_r{$headers{$header}} = $header;
+}
+
+1;
 
 
